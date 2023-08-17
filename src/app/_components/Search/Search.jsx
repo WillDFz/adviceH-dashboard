623a6487b3cde2fe1d19b316096eac7651489511
Will <willwow@outlook.com>
2023-08-17 17:58:33 -0300

    Update: Search icon

--- src/app/_components/Search/Search.jsx
+++ src/app/_components/Search/Search.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import styles from "./Search.module.scss";
 
 const Search = () => {
+    const [inputValue, setInputValue] = useState(null);
+    const [suggestions, setSuggestions] = useState(null);
+    
+
     return (
-        <div className="mb-4">
-            <div className="position-relative">
-                <input type="text" className="custom-input w-100" placeholder="Busca" />
-                <button className={`${styles.searchButton}`}>
-                    <img src="/svg/search.svg"  alt="" />
-                </button>
+        <>
+            <div className="mb-4">
+                <div className="position-relative">
+                    <input type="text" className="custom-input w-100" placeholder="Busca" />
+                    <button className={`${styles.searchButton}`}>
+                        <img src="/svg/search.svg" alt="" />
+                    </button>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
